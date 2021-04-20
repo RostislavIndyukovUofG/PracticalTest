b1fc23481541cbe5c4b5319073322f77f577dc59
Rostislav Indyukov <2538781I@student.gla.ac.uk>
2021-04-20 12:45:54 +0100

    Changed page styling.

--- newsapp/models.py
+++ newsapp/models.py
@@ -6,6 +6,9 @@ class Story(models.Model):
     pub_date = models.DateTimeField('date published')
     story_text = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name_plural = "Stories"
+
     def __str__(self):
         return self.heading
 
