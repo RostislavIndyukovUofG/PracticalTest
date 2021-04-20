42ff9acff4c455e044343801bd73f64bbfb4a585
Rostislav Indyukov <2538781I@student.gla.ac.uk>
2021-04-20 12:05:14 +0100

    practical test application.

--- newsapp/models.py
+++ newsapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Story(models.Model):
+    heading = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    story_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.heading
+
+
--- newsapp/views.py
+++ newsapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Story
+
+
+def index(request):
+    return render(request, 'newsapp/index.html')
+
+
+def news(request):
+    latest_news_list = Story.objects.order_by('pub_date')
+    context = {
+        'latest_news_list': latest_news_list,
+    }
+    return render(request, 'newsapp/news.html', context)
